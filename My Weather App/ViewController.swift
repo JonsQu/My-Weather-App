53cb892e2a69558960f1f07a65b6e7ae3b470c72
Joonatan <ShadoWalker@omistaja-macbook-pro.local>
2018-10-03 20:44:14 +0300

    Initial Commit

---
+++ My Weather App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My Weather App
+//
+//  Created by ShadoWalker on 3.10.2018.
+//  Copyright © 2018 ShadoWalker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
