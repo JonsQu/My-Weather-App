561fe3c69eb623fe55139e877bf437987ede8d09
JonsQu <joonatan.haavisto@cs.tamk.fi>
2018-10-04 23:51:21 +0300

    Added url fecthing

--- My Weather App/CurrentViewController.swift
+++ My Weather App/CurrentViewController.swift
@@ -15,9 +15,29 @@ class CurrentViewController: UIViewController {
     @IBOutlet weak var temperature: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.fetchUrl(url: "api.openweathermap.org/data/2.5/weather?q=Tampere")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    
+    func fetchUrl(url : String) {
+        let config = URLSessionConfiguration.default
+        
+        let session = URLSession(configuration: config)
+        
+        let url : URL? = URL(string: url)
+        
+        let task = session.dataTask(with: url!, completionHandler: doneFetching);
+        
+        // Starts the task, spawns a new thread and calls the callback function
+        task.resume();
+    }
+    
+    func doneFetching(data: Data?, response: URLResponse?, error: Error?) {
+        
+        // Execute stuff in UI thread
+        DispatchQueue.main.async(execute: {() in
+        })
+    }
 }
 
