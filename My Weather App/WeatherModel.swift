ccef82dc01c321144ab7c1c20663594cb4b32977
JonsQu <joonatan.haavisto@cs.tamk.fi>
2018-10-04 23:50:18 +0300

    Added model to data

---
+++ My Weather App/WeatherModel.swift
@@ -0,0 +1,12 @@
+//
+//  WeatherModel.swift
+//  My Weather App
+//
+//  Created by ShadoWalker on 4.10.2018.
+//  Copyright © 2018 ShadoWalker. All rights reserved.
+//
+
+import Foundation
+class WeatherModel: Decodable{
+    
+}
