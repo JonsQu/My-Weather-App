e007711015b156dc38e90dff76c52022888be473
JonsQu <joonatan.haavisto@cs.tamk.fi>
2018-10-11 20:43:41 +0300

    Add weatherModel support class Main

---
+++ My Weather App/Main.swift
@@ -0,0 +1,16 @@
+//
+//  Main.swift
+//  My Weather App
+//
+//  Created by ShadoWalker on 9.10.2018.
+//  Copyright © 2018 ShadoWalker. All rights reserved.
+//
+
+import Foundation
+class Main: Decodable{
+    var temp: Double?
+    var pressure: Double?
+    var humidity: Int?
+    var temp_min: Double?
+    var temp_max: Double?
+}
