97b3f7dd9b1da72e1c866e7d226b2fd6d6b8d3ee
JonsQu <joonatan.haavisto@cs.tamk.fi>
2018-10-11 20:43:20 +0300

    Add weatherModel support class Coord

---
+++ My Weather App/Coord.swift
@@ -0,0 +1,13 @@
+//
+//  Coord.swift
+//  My Weather App
+//
+//  Created by ShadoWalker on 9.10.2018.
+//  Copyright © 2018 ShadoWalker. All rights reserved.
+//
+
+import Foundation
+class Coord: Decodable{
+    var lon: Double?
+    var lat: Double?
+}
