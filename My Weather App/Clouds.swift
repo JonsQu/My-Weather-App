705cb3b4deb57840c7a4274478825a3a81e01114
JonsQu <joonatan.haavisto@cs.tamk.fi>
2018-10-11 20:41:50 +0300

    Add weatherModel support class

---
+++ My Weather App/Clouds.swift
@@ -0,0 +1,12 @@
+//
+//  Clouds.swift
+//  My Weather App
+//
+//  Created by ShadoWalker on 9.10.2018.
+//  Copyright © 2018 ShadoWalker. All rights reserved.
+//
+
+import Foundation
+class Clouds: Decodable{
+    var all: Double?
+}
